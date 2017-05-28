eb260097147f7e400a423e2628d02c31637ec97b
Carson McKinstry <carson.mckinstry@gmail.com>
2017-05-28 00:59:28 -0500

    Remove todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -86,6 +86,21 @@ describe('Reducers', () => {
       expect(res.length).toEqual(1);
       expect(res[0]).toEqual(todos[0]);
     });
+
+    it('should wipe todos on logout', () => {
+      let todos = [{
+        id: 111,
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }];
+      let action = {
+        type: 'LOGOUT'
+      };
+      let res = reducers.todosReducer(df(todos), df(action));
+      expect(res.length).toEqual(0);
+    });
   });
 
   describe('authReducer', () => {
