71d665d033e37c3aa904f03d8fc47dd87933402f
Carson McKinstry <carson.mckinstry@gmail.com>
2017-05-27 23:48:27 -0500

    Refactored router

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,20 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const {Provider} = require('react-redux');
-const {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
-import TodoApp from 'TodoApp';
-import Login from 'Login';
+const {hashHistory} = require('react-router');
 
 const actions = require('actions');
 const store = require('configureStore').configure();
-const TodoAPI = require('TodoAPI');
+import firebase from 'app/firebase/';
+import router from 'app/router/';
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    hashHistory.push('/todos');
+  } else {
+    hashHistory.push('/');
+  }
+});
 
 store.dispatch(actions.startAddTodos());
 
@@ -20,12 +26,7 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <Route path="todos" component={TodoApp}/>
-        <IndexRoute component={Login}/>
-      </Route>
-    </Router>
+    {router}
   </Provider>,
   document.querySelector('#app')
 );
